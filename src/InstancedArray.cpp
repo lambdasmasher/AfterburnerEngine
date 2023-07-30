76e8f0b57bd834e1746d26197f57a9a0bb924c86
Oscar <>
2023-07-30 09:11:59 -0400

    data structure for instanced arrays

---
+++ src/InstancedArray.cpp
@@ -0,0 +1,34 @@
+#include "InstancedArray.hpp"
+
+#define FLOATS_PER_INSTANCE 16
+#define BYTES_PER_INSTANCE (sizeof(float) * FLOATS_PER_INSTANCE)
+
+InstancedArray::InstancedArray(unsigned numInstances) {
+    glGenBuffers(1, &vboId);
+    data.resize(FLOATS_PER_INSTANCE * numInstances);
+}
+
+void InstancedArray::setupAttributes(Vao *vao, unsigned start) {
+    unsigned attribs[] = {4, 4, 4, 4};
+    vao->bind();
+    glBindBuffer(GL_ARRAY_BUFFER, vboId);
+    unsigned idx = start, offset = 0;
+    for (unsigned size : attribs) {
+        glVertexAttribPointer(idx, size, GL_FLOAT, false, BYTES_PER_INSTANCE, (void*)offset);
+        glVertexAttribDivisor(idx, 1);
+        idx++;
+        offset += size;
+    }
+}
+
+void InstancedArray::add(Entity *entity) {
+    for (int r = 0; r < 4; r++)
+        for (int c = 0; c < 4; c++)
+            data[ptr++] = entity->matrix[r][c];
+}
+
+void InstancedArray::flush() {
+    glBindBuffer(GL_ARRAY_BUFFER, vboId);
+    glBufferData(GL_ARRAY_BUFFER, ptr * sizeof(float), data.data(), GL_DYNAMIC_DRAW);
+    ptr = 0;
+}
---
+++ src/InstancedArray.hpp
@@ -0,0 +1,20 @@
+#ifndef INSTANCEDARRAY
+#define INSTANCEDARRAY
+
+#include "GLResource.hpp"
+#include "Scene.hpp"
+
+class InstancedArray : public GLResource {
+public:
+    InstancedArray(unsigned numInstances);
+    ~InstancedArray() { glDeleteBuffers(1, &vboId); }
+    void setupAttributes(Vao *vao, unsigned start);
+    void add(Entity *entity);
+    void flush();
+private:
+    GLuint vboId;
+    unsigned ptr = 0;
+    std::vector<float> data;
+};
+
+#endif /* INSTANCEDARRAY */
