899f398dd7b45b9521201d45c09b9bfc151a0e2d
Oscar <>
2023-08-08 17:57:07 -0400

    updated shadow map class

--- src/ShadowMap.hpp
+++ src/ShadowMap.hpp
@@ -7,6 +7,11 @@ class ShadowMap {
 public:
     ShadowMap(int cascades);
     ~ShadowMap();
+    inline void bindFbo() { glBindFramebuffer(GL_FRAMEBUFFER, fboId); }
+    inline void bindMapTexture(unsigned i) {
+        glActiveTexture(GL_TEXTURE0 + i);
+        glBindTexture(GL_TEXTURE_2D_ARRAY, texArrayId);
+    }
 private:
     const int cascades;
     GLuint fboId;
