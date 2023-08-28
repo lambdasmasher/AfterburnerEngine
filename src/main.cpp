de11de8d2a474ec7d5a18783cc411d2db8a6d226
Oscar <>
2023-08-28 13:41:21 -0400

    adjust ocean fog

--- src/main.cpp
+++ src/main.cpp
@@ -9,14 +9,14 @@ int main(void) {
             Texture::loadFromImage("res/ground.png"), "res/heightmap.bmp", 100.f, 15.f);
     scene.toLightVector = glm::vec3(0.f, 1.f, 0.5f);
     scene.atmosphereTexture = Texture::atmosphereCubemap(scene.toLightVector, 512);
-    scene.fogDensity = 0.0003f;
-    scene.fogGradient = 1.0f;
+    scene.fogDensity = 0.00025f;
+    scene.fogGradient = 4.0f;
 
     scene.fftwater = std::make_unique<FFTWater>(512, 1.25f, 38.0f, 2000.0f, glm::vec2(1, 1), 1.7f);
-    scene.fftwater->position = glm::vec3(-3200.f, 0.f, -3200.f);
-    scene.fftwater->numTiles = 128;
+    scene.fftwater->position = glm::vec3(-6400.f, 0.f, -6400.f);
+    scene.fftwater->numTiles = 256;
     scene.fftwater->tileSize = 50.f;
-    scene.fftwater->tiling = 80.f;
+    scene.fftwater->tiling = 160.f;
 
     scene.forest = std::make_unique<Forest>(scene.terrain.get());
     scene.shadowMap = std::make_unique<ShadowMap>(std::vector<float>{1.0f, 300.f, 1000.f, 5000.f});
