6e18a8fb72d5c22ee62b3e604ac476c7dd2c8a75
Oscar <>
2023-09-08 19:27:45 -0400

    adjust lighting

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main(void) {
     scene.camera = std::make_unique<Camera>(glm::vec3(800.f, 100.f, 800.f), 1.f, 5000.f, 50.f);
     scene.terrain = std::make_unique<Terrain>(glm::vec3(0.f, 0.f, 0.f), 32, 50.f, 100.f,
             Texture::loadFromImage("res/ground.png"), "res/heightmap.bmp", 100.f, 15.f);
-    scene.toLightVector = glm::vec3(0.f, 1.f, 0.5f);
+    scene.toLightVector = glm::vec3(0.f, 1.f, -0.5f);
     scene.atmosphereTexture = Texture::atmosphereCubemap(scene.toLightVector, 512);
     scene.fogDensity = 0.00025f;
     scene.fogGradient = 4.0f;
